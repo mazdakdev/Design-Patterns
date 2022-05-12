93661dbfb54ff832fb021da203bcffb3476c2c31
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-05-12 09:27:35 +0430

    Init Commit

---
+++ Singelton.py
@@ -0,0 +1,15 @@
+'''
+Creational:
+    Singelton
+'''
+
+class Singleton(type):
+    _instance = None
+
+    def __call__(self , *args , **kwargs):
+        if self._instance == None:
+            self._instance = super().__call__()
+        return self._instance
+
+class Db(metaclass=Singleton):
+    pass
