73547b10067a421f094a4c77fcac3b5d57713395
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-05-13 10:20:00 +0430

    Added unit tests

---
+++ Creational/tests/test_singleton.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append("..") # Adds higher directory to python modules path.
+from Creational.Singleton import Db
+import unittest
+
+
+class TestSingleton(unittest.TestCase):
+    def test_singleton(self):
+        """
+        Test that check if Singleton design pattern works proply 
+        (To check if 2 instances are equal)
+        """
+        db1 = Db()
+        db2 = Db()
+        self.assertEqual(db1 , db2)
+
+if __name__ == '__main__':
+    unittest.main()
