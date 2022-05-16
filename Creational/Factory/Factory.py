87e8b203a6528c91388cb00a969f48b3a6b9f280
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-05-16 14:53:09 +0430

    Finished Factory Design-pattern

--- Creational/Factory/Factory.py
+++ Creational/Factory/Factory.py
@@ -5,12 +5,11 @@ class IDevice(metaclass=ABCMeta):
     def get_hardware():
         """ InterFace """
 
-
-class DeviceMac(Ichair):
+class DeviceMac(IDevice):
     def __init__(self):
         self.cpu = "M1"
         self.memory = 16
-        self.color = "Black"
+        self.color = "Space-Gray"
         self.storage = 512
 
     def get_hardware(self):
@@ -21,5 +20,48 @@ class DeviceMac(Ichair):
             "Storage" : self.storage
         }
 
+class DeviceIphone(IDevice):
+    def __init__(self):
+        self.cpu = "A13"
+        self.memory = 4
+        self.color = "White"
+        self.storage = 128
+
+    def get_hardware(self):
+        return {
+            "CPU" : self.cpu,
+            "Memory" : self.memory,
+            "color": self.color,
+            "Storage" : self.storage
+        }
+
+class DeviceFactory():
+    @staticmethod
+    def get_device(device_type):
+        try:
+            if device_type == 0:
+                return DeviceMac()
+            
+            elif device_type == 1:
+                return DeviceIphone()
+            raise AssertionError("Your requested Device not found !")
+            
+        except AssertionError as e:
+            print(e)
 
 
+if __name__ == "__main__":
+    DeviceType = int(input(
+        """
+        Please select your desire device below :
+
+        -0 Mac
+        -1 Iphone
+
+        ==>
+        """
+    ))
+
+    Device = DeviceFactory.get_device(DeviceType)
+
+    print(Device.get_hardware())
